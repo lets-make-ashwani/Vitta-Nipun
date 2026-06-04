2a2c520e21d9934798f7c0ce3325115aa4b88ad8
lets-make-ashwani <ashvish420waniwakarma.com>
2026-06-04 15:04:14 +0530

    add footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -74,7 +74,7 @@ export function Footer() {
               </li>
               <li className="flex items-center gap-3">
                 <Phone size={20} className="text-secondary flex-shrink-0" />
-                <span className="text-white/70">+91 9956660896 , +91 9365106332</span>
+                <span className="text-white/70">+91 9956660896 ,</br> +91 9365106332</span>
               </li>
               <li className="flex items-center gap-3">
                 <Mail size={20} className="text-secondary flex-shrink-0" />
